57e645e97c0c1657e028937f3de67adcfda1306c
Manolis Chatzakis <manosxatzakiss2000@gmail.com>
2023-12-10 13:58:36 +0100

    added code to gather and save locally the results

--- run.py
+++ run.py
@@ -91,6 +91,14 @@ def run_standard(cfg, train_loader, val_loader, model):
         testing_time = time.time() - start_time
         results.append([split, acc_mean, acc_std, f"{testing_time:.2f}"])
 
+    test_summary_file = open(f"{cfg.results.dir}/test_summary.csv", "w")
+    test_summary_file.write(
+        "split,acc_mean,acc_std,testing_time\n"
+    )
+    for row in results:
+        test_summary_file.write(f"{row[0]},{row[1]},{row[2]},{row[3]}\n")
+    test_summary_file.close()
+
     print(f"Results logged to ./checkpoints/{cfg.exp.name}/results.txt")
 
     if cfg.mode == "train":
@@ -113,59 +121,118 @@ def run_bioMON(cfg, train_loader, val_loader, model):
     if cfg.mode == "train":
         (
             model,
-            total_training_time,
-            total_head_fit_time,
-            head_train_acc,
-            head_test_acc,
+            training_time,
             head_fit_time,
+            head_support_acc,
+            head_query_acc,
+            avg_head_fit_time,
         ) = train_bioMON(train_loader, val_loader, model, cfg)
 
         print("Training complete! Logging the results...")
         display_table = PrettyTable(
             [
-                "head_acc_train (mean)",
-                "head_acc_test (mean)",
-                "head_fit_time (mean)",
-                "total_head_fit_time",
-                "total_training_time",
+                "head_acc_support",
+                "head_acc_query",
+                "avg_head_fit_time (s)",
+                "total_head_fit_time (s)",
+                "total_training_time (s)",
             ]
         )
         display_table.add_row(
             [
-                head_train_acc,
-                head_test_acc,
-                head_fit_time,
-                total_head_fit_time,
-                total_training_time,
+                f"{head_support_acc:.2f}",
+                f"{head_query_acc:.2f}",
+                f"{avg_head_fit_time:.2f}",
+                f"{head_fit_time:.2f}",
+                f"{training_time:.2f}",
             ]
         )
         print(display_table)
 
     results = []
+    detailed_results =  []
     print("Checkpoint directory:", cfg.checkpoint.dir)
     for split in cfg.eval_split:
         start_time = time.time()
         (
             acc_mean,
             acc_std,
-            head_train_acc_mean,
-            head_train_acc_std,
-            head_test_acc_mean,
-            head_test_acc_std,
+            head_support_acc_mean,
+            head_support_acc_std,
+            head_query_acc_mean,
+            head_query_acc_std,
             head_fit_time_mean,
             head_fit_time_std,
-            total_head_fit_time
+            total_head_fit_time,
         ) = test_bioMON(cfg, model, split)
         testing_time = time.time() - start_time
-        results.append([split, acc_mean, acc_std, f"{testing_time:.2f}", f"{total_head_fit_time:.2f}", head_test_acc_mean, head_test_acc_std])
+        
+        results.append(
+            [
+                split,
+                acc_mean,
+                acc_std,
+                head_query_acc_mean,
+                head_query_acc_std,
+                total_head_fit_time,
+                testing_time,
+            ]
+        )
+        
+        detailed_results.append(
+            [
+                split,
+                acc_mean,
+                acc_std,
+                head_support_acc_mean,
+                head_support_acc_std,
+                head_query_acc_mean,
+                head_query_acc_std,
+                head_fit_time_mean,
+                head_fit_time_std,
+                total_head_fit_time,
+                testing_time,
+            ]
+        )
 
-    print(f"Results logged to ./checkpoints/{cfg.exp.name}/results.txt")
+    
+    test_summary_file = open(f"{cfg.results.dir}/test_summary.csv", "w")
+    test_summary_file.write(
+        "split,acc_mean,acc_std,head_support_acc_mean,head_support_acc_std,head_query_acc_mean,head_query_acc_std,head_fit_time_mean,head_fit_time_std,total_head_fit_time,testing_time\n"
+    )
+    for row in detailed_results:
+        test_summary_file.write(f"{row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]},{row[7]},{row[8]},{row[9]},{row[10]}\n")
+    test_summary_file.close()
+    
+    print(f"Detailed results logged to {cfg.results.dir}/test_summary.csv")
+    print(f"Single line results logged to ./checkpoints/{cfg.exp.name}/results.txt")
 
     if cfg.mode == "train":
-        table = wandb.Table(data=results, columns=["split", "acc_mean", "acc_std", "test time(s)", "head fit time(s)", "head_test_acc_mean", "head_test_acc_std"])
+        table = wandb.Table(
+            data=results,
+            columns=[
+                "split",
+                "acc_mean",
+                "acc_std",
+                "acc_head_query_mean",
+                "acc_head_query_std",
+                "head fit time (s)",
+                "testing time (s)",
+            ],
+        )  # "test time(s)", "head fit time(s)", "head_test_acc_mean", "head_test_acc_std"])
         wandb.log({"eval_results": table})
 
-    display_table = PrettyTable(["split", "acc_mean", "acc_std","test time(s)", "head fit time(s)", "head_test_acc_mean", "head_test_acc_std"])
+    display_table = PrettyTable(
+        [
+            "split",
+            "acc_mean",
+            "acc_std",
+            "acc_head_query_mean",
+            "acc_head_query_std",
+            "head fit time (s)",
+            "testing time (s)",
+        ]
+    )
     for row in results:
         display_table.add_row(row)
     print(display_table)
@@ -306,6 +373,9 @@ def train_bioMON(train_loader, val_loader, model, cfg):
     if not os.path.isdir(cp_dir):
         os.makedirs(cp_dir)
 
+    if not os.path.isdir(cfg.results.dir):
+        os.makedirs(cfg.results.dir)
+
     wandb.init(
         project=cfg.wandb.project,
         entity=cfg.wandb.entity,
@@ -340,7 +410,11 @@ def train_bioMON(train_loader, val_loader, model, cfg):
 
     all_head_train_acc = 0
     all_head_test_acc = 0
-    all_head_fit_time = 0
+
+    train_results_file = open(f"{cfg.results.dir}/epoch_train_results.csv", "w")
+    train_results_file.write(
+        "epoch,train_loss,val_loss,val_acc,val_head_support_acc,val_head_query_acc\n"
+    )
 
     for epoch in range(cfg.method.start_epoch, cfg.method.stop_epoch):
         wandb.log({"epoch": epoch})
@@ -348,28 +422,39 @@ def train_bioMON(train_loader, val_loader, model, cfg):
 
         training_start_time = time.time()
         (
-            avg_loss,
-            avg_head_train_acc,
-            avg_head_test_acc,
-            avg_head_fit_time,
-            total_loop_head_fit_time,
+            train_loss,
+            train_head_support_acc,
+            train_head_query_acc,
+            _,
+            train_head_fit_time,
         ) = model.train_loop(epoch, train_loader, optimizer)
         total_training_time += time.time() - training_start_time
 
-        total_head_fit_time += total_loop_head_fit_time
-        all_head_train_acc += avg_head_train_acc
-        all_head_test_acc += avg_head_test_acc
-        all_head_fit_time += avg_head_fit_time
+        total_head_fit_time += train_head_fit_time
+        all_head_train_acc += train_head_support_acc
+        all_head_test_acc += train_head_query_acc
 
         if epoch % cfg.exp.val_freq == 0 or epoch == cfg.method.stop_epoch - 1:
             model.eval()
-            acc, head_train_acc, head_test_acc, head_fit_time, loss_mean, total_head_fit_time = model.test_loop(val_loader)
+            (
+                acc,
+                head_support_acc,
+                head_query_acc,
+                head_fit_time,
+                val_loss,
+                _,
+            ) = model.test_loop(val_loader)
 
             print(f"Epoch {epoch}: {acc:.2f}")
-            wandb.log({"acc/val": acc})
-            wandb.log({"acc/head_train": head_train_acc})
-            wandb.log({"acc/head_test": head_test_acc})
-            wandb.log({"time/head_fit_time": head_fit_time})
+            wandb.log({"validation acc": acc})
+            wandb.log({"validation support acc": head_support_acc})
+            wandb.log({"validation query acc": head_query_acc})
+            wandb.log({"validation fit time": head_fit_time})
+
+            # Log training metrics
+            # (epoch,train_loss,val_loss,val_acc,val_head_support_acc,val_head_query_acc)
+            epoch_data = f"{epoch},{train_loss},{val_loss},{acc},{head_support_acc},{head_query_acc}\n"
+            train_results_file.write(epoch_data)
 
             if acc > max_acc:
                 print("best model! save...")
@@ -380,18 +465,36 @@ def train_bioMON(train_loader, val_loader, model, cfg):
         if epoch % cfg.exp.save_freq == 0 or epoch == cfg.method.stop_epoch - 1:
             outfile = os.path.join(cp_dir, "{:d}.tar".format(epoch))
             torch.save({"epoch": epoch, "state": model.state_dict()}, outfile)
-    
-    all_head_train_acc = all_head_train_acc / (cfg.method.stop_epoch - cfg.method.start_epoch)
-    all_head_test_acc = all_head_test_acc / (cfg.method.stop_epoch - cfg.method.start_epoch)
-    all_head_fit_time = all_head_fit_time / (cfg.method.stop_epoch - cfg.method.start_epoch)
+
+    train_results_file.close()
+
+    all_head_train_acc = all_head_train_acc / (
+        cfg.method.stop_epoch - cfg.method.start_epoch
+    )
+    all_head_test_acc = all_head_test_acc / (
+        cfg.method.stop_epoch - cfg.method.start_epoch
+    )
+    all_head_fit_time = total_head_fit_time / (
+        cfg.method.stop_epoch - cfg.method.start_epoch
+    )
+
+    train_summary_file = open(f"{cfg.results.dir}/train_summary.csv", "w")
+    train_summary_file.write(
+        "total_training_time,total_head_fit_time,train_head_support_acc,train_head_query_acc,avg_head_fit_time\n"
+    )
+    train_summary_file.write(
+        f"{total_training_time},{total_head_fit_time},{all_head_train_acc},{all_head_test_acc},{all_head_fit_time}\n"
+    )
+
+    train_summary_file.close()
 
     return (
         model,
         total_training_time,
         total_head_fit_time,
-        head_train_acc,
-        head_test_acc,
-        head_fit_time,
+        all_head_train_acc,
+        all_head_test_acc,
+        all_head_fit_time,
     )
 
 
@@ -419,15 +522,15 @@ def test_bioMON(cfg, model, split):
         (
             acc_mean,
             acc_std,
-            head_train_acc_mean,
-            head_train_acc_std,
-            head_test_acc_mean,
-            head_test_acc_std,
+            head_support_acc_mean,
+            head_support_acc_std,
+            head_query_acc_mean,
+            head_query_acc_std,
             head_fit_time_mean,
             head_fit_time_std,
             loss_mean,
             loss_std,
-            total_head_fit_time
+            total_head_fit_time,
         ) = model.test_loop(test_loader, return_std=True)
 
     with open(f"./checkpoints/{cfg.exp.name}/results.txt", "a") as f:
@@ -449,17 +552,19 @@ def test_bioMON(cfg, model, split):
             "Time: %s, Setting: %s, Acc: %s, Model: %s \n"
             % (timestamp, exp_setting, acc_str, model_file)
         )
+    
+    
 
     return (
         acc_mean,
         acc_std,
-        head_train_acc_mean,
-        head_train_acc_std,
-        head_test_acc_mean,
-        head_test_acc_std,
+        head_support_acc_mean,
+        head_support_acc_std,
+        head_query_acc_mean,
+        head_query_acc_std,
         head_fit_time_mean,
         head_fit_time_std,
-        total_head_fit_time
+        total_head_fit_time,
     )
 
 
